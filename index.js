fdecdb90fb2e7f091f53a35af9b2887c3e62005c
Kyle Bishop <kybishop@gmail.com>
2017-05-04 13:47:01 -0700

    upgrade deps

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-attacher',
-
-  included: function(/* app */) {
-    this._super.included.apply(this, arguments);
-  }
+  name: 'ember-attacher'
 };
