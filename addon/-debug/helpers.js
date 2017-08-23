6a5b97d8c8f2350a30ea476e38eba2fc08594791
Kyle Bishop <kybishop@gmail.com>
2017-08-23 16:07:21 -0700

    chore(cleanup) remove whitespace and obsolete debug helpers

--- addon/-debug/helpers.js
+++ addon/-debug/helpers.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
-import { deprecate as emberDeprecate } from '@ember/application/deprecations';
-import { warn as emberWarn } from '@ember/debug';
+const { Logger } = Ember;
 
-const {
-  Logger
-} = Ember;
+export function assert(msg, conditional) {
+  if (!conditional) {
+    throw new Error(msg);
+  }
+}
 
 export function debug() {
   Logger.debug(...arguments);
@@ -15,20 +16,6 @@ export function debugOnError(msg, conditional) {
     console.error(msg); // eslint-disable-line no-console
     debugger; // eslint-disable-line no-debugger
   }
-}
-
-export function assert(msg, conditional) {
-  if (!conditional) {
-    throw new Error(msg);
-  }
-}
-
-export function warn() {
-  emberWarn(...arguments);
-}
-
-export function deprecate() {
-  emberDeprecate(...arguments);
 }
 
 export function stripInProduction(cb) {
