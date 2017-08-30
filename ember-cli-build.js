d16688d25f16ebf4c192dfeb19632fba7f8170b4
Kyle Bishop <kybishop@gmail.com>
2017-08-30 12:48:20 -0700

    chore(remove jquery) (#56)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,13 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
-    // Add options here
-  });
+  const options = {};
+
+  if (defaults.project.findAddonByName('ember-native-dom-event-dispatcher')) {
+    options.vendorFiles = { 'jquery.js': null };
+  }
+
+  const app = new EmberAddon(defaults, options);
 
   /*
     This build file specifies the options for the dummy test app of this
