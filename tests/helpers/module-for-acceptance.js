2773b30f7075338b59552bebdf417cd1352e1582
Kyle Bishop <kybishop@gmail.com>
2017-09-01 18:06:34 -0700

    chore(bump deps) ember-cli, ember-popper, etc. (#61)

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,7 +1,9 @@
 import { module } from 'qunit';
-import RSVP from 'rsvp';
+import Ember from 'ember';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
+
+const { RSVP: { resolve } } = Ember;
 
 export default function(name, options = {}) {
   module(name, {
@@ -15,7 +17,7 @@ export default function(name, options = {}) {
 
     afterEach() {
       const afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return RSVP.resolve(afterEach).then(() => destroyApp(this.application));
+      return resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
 }
