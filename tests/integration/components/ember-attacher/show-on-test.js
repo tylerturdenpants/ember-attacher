897ecdfec18c85298fa6626cbaed21f7356f3a8d
Kyle Bishop <kybishop@gmail.com>
2017-11-23 13:38:53 -0800

    fix(hideOn/showOn set to '' should set them to null) (#89)

--- tests/integration/components/ember-attacher/show-on-test.js
+++ tests/integration/components/ember-attacher/show-on-test.js
@@ -62,3 +62,29 @@ test('sets showOn to an empty array when passed `null`', async function(assert) 
 
   assert.equal(isVisible(attachment), false, 'Still hidden after focus');
 });
+
+test("sets showOn to an empty array when passed `''` (empty string)", async function(assert) {
+  assert.expect(3);
+
+  this.set('showOn', '');
+
+  this.render(hbs`
+      <button id="target">
+        {{#attach-popover id='attachment' hideOn='click' showOn=showOn}}
+          showOn mouseenter
+        {{/attach-popover}}
+      </button>
+    `);
+
+  const attachment = find('#attachment');
+
+  assert.equal(isVisible(attachment), false, 'Initially hidden');
+
+  await triggerEvent('#target', 'mouseenter');
+
+  assert.equal(isVisible(attachment), false, 'Still hidden after mouseenter');
+
+  await triggerEvent('#target', 'focus');
+
+  assert.equal(isVisible(attachment), false, 'Still hidden after focus');
+});
