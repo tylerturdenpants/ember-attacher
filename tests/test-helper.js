41244468eec8b198ac480c44e9e80a168e826a81
Kyle Bishop <kybishop@gmail.com>
2017-12-03 16:17:20 -0800

    chore(bump deps) ember 2.17, etc. (#98)

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,11 +1,10 @@
-import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
-import { start } from 'ember-cli-qunit';
+import Application from '../app';
 import { registerWaiter } from 'ember-raf-test-waiter';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
 
 registerWaiter();
 
-setResolver(resolver);
+setApplication(Application.create({ autoboot: false }));
+
 start();
