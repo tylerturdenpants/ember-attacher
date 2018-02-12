86e774949d3145ce26286f7d50a1470e6963a238
Kyle Bishop <kybishop@gmail.com>
2018-02-11 20:55:34 -0800

    chore(bump deps) ember 2.18, etc.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,10 +1,11 @@
 import Application from '../app';
 import { registerWaiter } from 'ember-raf-test-waiter';
+import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
 registerWaiter();
 
-setApplication(Application.create({ autoboot: false }));
+setApplication(Application.create(config.APP));
 
 start();
