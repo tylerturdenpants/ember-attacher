7d399bf67b5ab9038b8df7e6a5d8e91b51c85aa8
Richard Viney <richard@lightspeedgraphics.co.nz>
2019-01-06 12:00:31 +1300

    Add test for useCapture=true

---
+++ tests/integration/components/ember-attacher/use-capture-test.js
@@ -0,0 +1,37 @@
+import hbs from 'htmlbars-inline-precompile';
+import { click } from 'ember-native-dom-helpers';
+import { isVisible } from 'ember-attacher';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+
+import { render } from '@ember/test-helpers';
+
+module('Integration | Component | useCapture "true"', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('hides a popover when the clicked element does not bubble the click event', async function(assert) {
+    assert.expect(2);
+
+    await render(hbs`
+      <div id="click-out-target" {{action (mut unusedVar) bubbles=false}}>
+      </div>
+
+      <button>
+        Click me, captain!
+
+        {{#attach-popover id='attachment'
+                          hideOn='clickout'
+                          isShown=true
+                          useCapture=true}}
+          hideOn clickout
+        {{/attach-popover}}
+      </button>
+    `);
+
+    assert.equal(isVisible('#attachment'), true, 'Initially shown');
+
+    await click('#click-out-target');
+
+    assert.equal(isVisible('#attachment'), false, 'Now hidden');
+  });
+});
