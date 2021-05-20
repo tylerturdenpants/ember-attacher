a30d9e00a7d0acba3d3dbc79a4db01dfddbee428
Ryan Mark <rgmark@gmail.com>
2021-05-20 15:31:09 -0700

    update to ember-qunit 5 (#427)
    * update to ember-qunit 5
    
    * bump

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,9 +3,14 @@ import { registerWaiter } from 'ember-raf-test-waiter';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 
-registerWaiter();
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
+
+registerWaiter();
 
 start();
