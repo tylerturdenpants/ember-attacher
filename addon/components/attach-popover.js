d591745b4a82db4d89cda78fd97c14923b63302e
Petro Zubar <36226946+pzubar@users.noreply.github.com>
2023-06-21 16:50:38 +0300

    Fix the "cancelAnimationFrame is not defined" error in fastboot (#820)
    * Fixes the "cancelAnimationFrame is not defined" error in fastboot
    
    * Adds the safer "typeOf" check
    
    * Bumps the node version to make CI green
    
    * Update addon/components/attach-popover.js
    
    Co-authored-by: Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
    
    ---------
    
    Co-authored-by: Dmitry Krasnoukhov <dmitry@krasnoukhov.com>

--- addon/components/attach-popover.js
+++ addon/components/attach-popover.js
@@ -832,6 +832,10 @@ export default class AttachPopover extends Component {
   }
 
   _cancelAnimation() {
+    if (typeof cancelAnimationFrame !== 'function') {
+      return;
+    }
+
     cancelAnimationFrame(this._animationTimeout);
 
     stripInProduction(() => {
